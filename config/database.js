fd4b566873017c4e1536e2c3450611c6ff39f710
nugnug7 <nugnug.develop777@gmail.com>
2024-10-11 18:01:01 +0700

    Membuat folder config dan file database untuk koneksi database

---
+++ config/database.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2')
+// Menyambungkan ke sql 
+const db = mysql.createConnection ({
+    host        : 'localhost',
+    user        : 'root',
+    password    : '',
+    database    : 'db_jdf_sep24'
+})
+
+// Buka koneksi Database
+db.connect ()
+
+function eksekusi (script_sql_dan_data) {
+    return new Promise((resolve, reject) => {
+        db.query(script_sql_dan_data, function(errorSql, hasil) {
+            if (errorSql) {
+                reject(errorSql)   
+            } else {
+                resolve(hasil)
+            }
+         })
+    })
+}
--- controller/c_auth.js
+++ controller/c_auth.js
@@ -2,5 +2,17 @@ module.exports =
 {
     halaman_login: function(req,res) {
     res.render('v_auth/login')
+    },
+    
+    proses_login: function(req,res) {
+        // ambil inputan dari form login
+        let form_email = req.body.form_email
+        let form_password = req.body.form_password
+
+        // cek email yang di input, ada di database
+
+
+
     }
+
 }
