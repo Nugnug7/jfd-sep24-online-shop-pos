e117ab54c65ebad31fa6e019f0d00806c8206358
nugnug7 <nugnug.develop777@gmail.com>
2024-10-11 20:56:59 +0700

    Mengarahkan ke halaman sistem utama setelah berhasil memasukan emaik dan password dengan benar

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express =require('express')
-const app = express()
-const port = 3000
-const c_beranda =require('./controller/c_beranda')
-const c_auth =require('./controller/c_auth')
+const express       = require('express')
+const app           = express()
+const port          = 3000
+const c_beranda     = require('./controller/c_beranda')
+const c_auth        = require('./controller/c_auth')
+const c_toko        = require('./controller/c_toko')
 
 app.use( express.urlencoded({extended:false}))
 app.use(express.static('public'))
@@ -13,6 +14,8 @@ app.set('views', './view')
 app.get('/', c_beranda.halaman_awal)
 app.get('/auth/login' ,c_auth.halaman_login)
 app.post('/auth/proses-login' ,c_auth.proses_login)
+
+app.get('/toko' ,c_toko.index)
 
 
 app.listen(port, ()=>{
--- controller/c_auth.js
+++ controller/c_auth.js
@@ -1,5 +1,5 @@
+const bcrypt     = require('bcryptjs')
 const model_user = require('../model/m_user')
-
 
 module.exports = 
 {
@@ -10,19 +10,26 @@ module.exports =
         res.render('v_auth/login', data)
     },
     
-
     proses_login: async function(req,res) {
         // ambil inputan dari form login
         let form_email = req.body.form_email
         let form_password = req.body.form_password
 
-        // cek email yang di input, ada di databas
-        let email_exist = model_user.cari_email(form_email)
-        if (email_exist.length > 0) {
+        // cek email yang di input, ada di database
+        let email_exist = await model_user.cari_email(form_email)
+            if (email_exist.length > 0) {
         // Cek Password
-            res.send('email ada')
+            let password_cocok = bcrypt.compareSync(form_password, email_exist[0].password)
+            if (password_cocok ) {
+                // Arahkan ke halaman utama sistem
+                res.redirect('/toko')
+            } else {
+                // tendang ke halaman login
+                let pesan = (`Password salah`)
+                res.redirect(`/auth/login?notif= ${pesan}`)
+            }
         } else {
-        // Tendang ke halaman register
+            // Tendang ke halaman register
         let pesan = (`Email anda belum terdaftar, silahkan register lebih dulu`)
         res.redirect(`/auth/login?notif= ${pesan}`)
         }
