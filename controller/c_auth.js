e9b1805927f05c206f90fb027c05021380420c9d
nugnug7 <nugnug.develop777@gmail.com>
2024-10-11 18:04:19 +0700

    Menambah form login di file c_auth

--- controller/c_auth.js
+++ controller/c_auth.js
@@ -9,10 +9,7 @@ module.exports =
         let form_email = req.body.form_email
         let form_password = req.body.form_password
 
-        // cek email yang di input, ada di database
-
-
-
+        // cek email yang di input, ada di databas
     }
 
 }
