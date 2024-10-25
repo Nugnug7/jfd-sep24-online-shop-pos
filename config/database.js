b22724da33e7e28db64952519003af56ce902051
nugnug7 <nugnug.develop777@gmail.com>
2024-10-25 20:05:50 +0700

    hitung kalkulasi belanjaan

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection ({
     host        : 'localhost',
     user        : 'root',
     password    : '',
-    database    : 'db_sep24_jdf',
+    database    : 'db_sep2024_jdf',
 })
 
 // Buka koneksi Database
